4af106f2caa26532c53816949b05c2665148cfba
Liam Pickering <61335075+Liam-Pickering@users.noreply.github.com>
2022-02-07 16:13:38 +0000

    Delete Flask_Code directory

--- Flask_Code/flask-introduction/random.py
+++
@@ -1,8 +0,0 @@
-from flask import request
-
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        return 'This is a POST request'
-    else:
-        return 'This is a GET request'
